14d2b02d0eeabc717ed6b3e3a126627c7894c4bd
Mitul1927 <124992398+Mitul1927@users.noreply.github.com>
2023-06-06 22:28:25 +0530

    Add files via upload

---
+++ Day8 leetcode question/shuffle_the_array.java
@@ -0,0 +1,28 @@
+public class shuffle_the_array {
+        public int[] shuffle(int[] nums, int n) {
+            int[] nums3 = new int[nums.length];
+            int count =0;
+            if(nums.length==2)
+            {
+                nums3[0]=nums[0];
+                nums3[1]=nums[1];
+            }
+            else{
+                for(int i=0;i<nums.length/2;i++)
+                {
+                    nums3[2*i]=nums[i];
+
+                }
+
+                for(int i=0;i<nums.length/2;i++)
+                {
+                    nums3[2*i+1]=nums[i+n];
+                }
+
+            }
+
+
+            return nums3;
+        }
+    }
+
