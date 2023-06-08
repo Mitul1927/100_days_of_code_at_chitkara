02ae5ed40c4a0c8d96f0d98167a42c65f26669e1
Mitul1927 <124992398+Mitul1927@users.noreply.github.com>
2023-06-08 23:05:06 +0530

    Add files via upload

---
+++ Day10 leetcode question/number_of_good_pairs.java
@@ -0,0 +1,13 @@
+public class number_of_good_pairs {
+    public int numIdenticalPairs(int[] nums) {
+        int num=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    num++;
+                }
+            }
+        }
+        return num;
+    }
+}
