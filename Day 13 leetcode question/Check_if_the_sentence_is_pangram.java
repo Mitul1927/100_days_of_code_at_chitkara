c06294f5c6c3a56b7cb85162c341cf5199c4c1e9
Mitul1927 <124992398+Mitul1927@users.noreply.github.com>
2023-06-18 12:46:01 +0530

    Add files via upload

---
+++ Day 13 leetcode question/Check_if_the_sentence_is_pangram.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Check_if_the_sentence_is_pangram {
+    public boolean checkIfPangram(String sentence) {
+        char c[] = sentence.toCharArray();
+        Arrays.sort(c);
+        String s=new String(c);
+        String a="abcdefghijklmnopqrstuvwxyz";
+        int k=0;
+        for(int i=0;i<s.length();i++){
+            if(k==26){
+                break;
+            }
+            if(s.charAt(i)==a.charAt(k)){
+                k++;
+            }
+        }
+        if(k==26){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
