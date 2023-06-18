1b7b743fe827d155da320a53d4e7b772b16ddfdd
Mitul1927 <124992398+Mitul1927@users.noreply.github.com>
2023-06-18 12:48:10 +0530

    Add files via upload

---
+++ Day 14 Leetcode question/Count_Items_matching_a_rule.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class Count_Items_matching_a_rule {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int count=0;
+        for(int i =0;i<items.size();i++){
+            if(ruleKey.equals("type")){
+                if(ruleValue.equals(items.get(i).get(0))){
+                    count++;
+                }
+            }
+            else if(ruleKey.equals("color")){
+                if(ruleValue.equals(items.get(i).get(1))){
+                    count++;
+                }
+            }
+            else{
+                if(ruleValue.equals(items.get(i).get(2))){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
