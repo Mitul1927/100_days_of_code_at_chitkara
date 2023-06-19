7ef607331bb93f15bffd050c1699215389e46b1a
Mitul1927 <124992398+Mitul1927@users.noreply.github.com>
2023-06-19 20:42:20 +0530

    Add files via upload

---
+++ Day 20 leetcode question/Tranpose_of_a_matrix.java
@@ -0,0 +1,11 @@
+public class Tranpose_of_a_matrix {
+    public int[][] transpose(int[][] matrix) {
+        int[][] answer=new int[matrix[0].length][matrix.length];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                answer[j][i]=matrix[i][j];
+            }
+        }
+        return answer;
+    }
+}
