1cfde7d5098f9ef400951ebc41b44b45eb78dbdd
Mitul1927 <124992398+Mitul1927@users.noreply.github.com>
2023-06-20 15:39:07 +0530

    Add files via upload

---
+++ Day 21 Leetcode question/Add_to_array_form_of_integer.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Add_to_array_form_of_integer {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> ans=new LinkedList<>();
+        for(int i=num.length-1;i>=0;i--){
+            ans.add(0,(num[i]+k)%10);
+            k=(num[i]+k)/10;
+        }
+        while(k>0){
+            ans.add(0,k%10);
+            k/=10;
+        }
+        return ans;
+    }
+}
