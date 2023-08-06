1a1f6b4e6daedcf01c8906883827707be45a5999
Mitul1927 <124992398+Mitul1927@users.noreply.github.com>
2023-08-06 14:55:17 +0530

    Create Find_minimum_in_rotated_array.java

---
+++ Day 66 Leetcode question/Find_minimum_in_rotated_array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int s=0;
+        int e=nums.length-1;
+        int min=2222;
+        while(s<=e){
+            int m=(s+e)/2;
+            if(nums[m]<nums[e]){
+                e=m;
+            }
+            else{
+                s=m+1;
+            }
+        }
+        return nums[e];
+    }
+}
